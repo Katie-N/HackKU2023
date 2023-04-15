e5f34e38bf7497fdf97d4313fc0fc4d117e3964c
Katie N <knordber@stumail.jccc.edu>
2023-04-15 01:45:58 -0500

    Barebones website

---
+++ scripts/script.js
@@ -0,0 +1,47 @@
+function createPlot(numSquares) {
+   let gardenPlot = document.getElementById("gardenPlot")
+   removeAllChildNodes(gardenPlot);
+   for (i = 0; i < numSquares; i++) {
+     let square = document.createElement("div");
+     document.getElementById("gardenPlot").appendChild(square);
+   }
+}
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+      parent.removeChild(parent.firstChild);
+    }
+}
+
+let widthInput = document.getElementById("width");
+let lengthInput = document.getElementById("length")
+
+let width = 1;
+let length = 1;
+
+createPlot(width*length);
+
+lengthInput.addEventListener("input", updateLength);
+widthInput.addEventListener("input", updateWidth);
+
+function updateLength() {
+  console.log("Updating Length");
+  let rows = "";
+  length = lengthInput.value;
+  for (let i = 0; i < length; i++) {
+    rows += " 1fr"
+}
+document.getElementById("gardenPlot").style.gridTemplateRows = rows;
+  createPlot(width * length);
+}
+
+function updateWidth() {
+  console.log("Updating Width");
+  let columns = "";
+  width = widthInput.value;
+  for (let i = 0; i < width; i++) {
+    columns += " 1fr"
+  }
+  document.getElementById("gardenPlot").style.gridTemplateColumns = columns;
+    createPlot(width * length);
+}
